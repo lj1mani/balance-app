957710c90a70a020237361bcbd2728ae09901a36
lj1mani <ljimani@gmail.com>
2025-09-06 14:35:13 +0200

    little change

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,18 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+import java.sql.Date;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 public class DatabaseManager {
 
+    /*
     // Database connection details
     private static final String URL = "jdbc:mariadb://localhost:3306/balance_db";
     private static final String USER = "root";
     private static final String PASSWORD = "database";
+*/
+    private static final String URL;
+    private static final String USER;
+    private static final String PASSWORD;
+
+    // Load database config from db.properties
+    static {
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream("db.properties")) {
+            props.load(fis);
+            URL = props.getProperty("DB_URL");
+            USER = props.getProperty("DB_USER");
+            PASSWORD = props.getProperty("DB_PASSWORD");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Could not load database configuration. Please check db.properties.");
+        }
+    }
+
 
     // Returns a connection to the database
     public static Connection getConnection() throws SQLException {
